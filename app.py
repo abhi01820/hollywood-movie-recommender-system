3ba15a54e95871667972e0408062b0d1e9626e7c
Mekala Abhilash <ugs23115_cse.abhilash@cbit.org.in>
2025-12-30 15:40:07 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import streamlit as st
+import pickle
+import pandas
+
+
+movies_dict=pickle.load(open('movie_dict.pkl','rb'))
+similarity=pickle.load(open('similarity.pkl','rb'))
+movies=pd.DataFrame(movies_dict)
+
+
+
+
+
+def recommender(movie):
+    movie_index=movies[movies['title']==movie].index[0]
+    distances=similarity[movie_index]
+    movies_list=sorted(list(enumerate(distances)),reverse=True,key=lambda x:x[1])[1:11]
+
+    recommended_movies=[]
+    for i in movies_list :
+        movie_id=i[0]
+        recommended_movies.append(movies.iloc[i[0]].title)
+    return recommended_movies
+
+st.title('Movie Recommender System')
+
+selected_movie_name=st.selectbox(
+    "How are you babe  ? ",
+    movies['title'].values
+)
+
+if st.button('Recommend '):
+    recommendations=recommender(selected_movie_name)
+    for i in recommendations:
+        st.write(i)
